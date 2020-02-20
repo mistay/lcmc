9d244c56de539d1af72414a4d3bd841185037c81
armin langhofer <armin@langhofer.net>
2020-02-20 15:36:48 +0100

    foo

--- lcmc/Program.cs
+++ lcmc/Program.cs
@@ -4,25 +4,176 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace lcmc
 {
     class Program
     {
-        static List<StateObject> stateObjects = new List<StateObject>();
-        static void Main(string[] args)
+        static IPEndPoint ipeAgent = null;
+        private static IPEndPoint ipeConsumer = null;
+
+        public static void agentHandler(object data)
         {
 
-            Console.WriteLine("args, len: " + args.Length);
-            for (int i=0; i<args.Length; i++)
+            if (!(data is Socket))
+                return;
+
+            Socket handler = (Socket)data;
+
+
+            byte[] buffer = new Byte[1024];
+
+            try
             {
-                Console.WriteLine("args[{0}]: {1}", i, args[i]);
+                int bytesRec = 0;
+                while ((bytesRec = handler.Receive(buffer)) > 0)
+                {
+                    foreach (Socket s in consumerSockets)
+                    {
+                        if (
+                            ((IPEndPoint)s.LocalEndPoint).Port + 1000 == ((IPEndPoint)handler.LocalEndPoint).Port ||
+                            ((IPEndPoint)s.LocalEndPoint).Port - 1000 == ((IPEndPoint)handler.LocalEndPoint).Port
+                            )
+                        {
+                            Console.WriteLine("-C-> {0} bytes", bytesRec);
+                            s.Send(buffer, bytesRec, SocketFlags.None);
+                            break;
+                        }
+                    }
+                }
+            } catch (Exception e)
+            {
+                // ignore
             }
 
-            IPEndPoint ipeAgents = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
-            IPEndPoint ipeEngineer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11001);
+            try
+            {
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                // ignore
+            }
+        }
 
+        static List<Socket> agentSockets = new List<Socket>();
+        static List<Socket> consumerSockets = new List<Socket>();
+
+        public static void listenForAgents()
+        {
+            byte[] bytes = new Byte[1024];
+
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+            while(true)
+            {
+                try
+                {
+                    listener.Bind(ipeAgent);
+                    listener.Listen(100);
+
+                    Console.WriteLine("listen4agents: " + ((IPEndPoint)listener.LocalEndPoint).ToString());
+
+                    while (true)
+                    {
+                        Socket handler = listener.Accept();
+                        agentSockets.Add(handler);
+                        Console.WriteLine("agent connected: " + ((IPEndPoint)handler.RemoteEndPoint).ToString());
+
+                        Thread t = new Thread(agentHandler);
+                        t.Start(handler);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+        }
+
+        public static void consumerHandler(object data)
+        {
+            if (!(data is Socket))
+                return;
+            Socket handler = (Socket)data;
+
+            byte[] buffer = new Byte[1024];
+
+            try
+            {
+                int bytesRec = 0;
+                while ((bytesRec = handler.Receive(buffer)) > 0)
+                {
+                    foreach (Socket s in agentSockets)
+                    {
+                        if (
+                        ((IPEndPoint)s.LocalEndPoint).Port + 1000 == ((IPEndPoint)handler.LocalEndPoint).Port ||
+                        ((IPEndPoint)s.LocalEndPoint).Port - 1000 == ((IPEndPoint)handler.LocalEndPoint).Port 
+                        )
+                        {
+                            // found corresponding socket
+                            Console.WriteLine("<A-- {0} bytes", bytesRec);
+                            s.Send(buffer, bytesRec, SocketFlags.None);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // ignore
+            }
+
+            try
+            {
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                // ignore
+            }
+        }
+
+
+
+        public static void listenForConsumers()
+        {
+            byte[] bytes = new Byte[1024];
+
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(ipeConsumer);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    Socket handler = listener.Accept();
+                    consumerSockets.Add(handler);
+                    Console.WriteLine("consumer connected: " + ((IPEndPoint)handler.RemoteEndPoint).ToString());
+
+                    Thread t = new Thread(consumerHandler);
+                    t.Start(handler);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
             if (args.Length >= 1)
             {
                 try
@@ -31,7 +182,7 @@ namespace lcmc
                     IPAddress iPAddress = IPAddress.Parse(s[0]);
                     int p = int.Parse(s[1]);
 
-                    ipeAgents = new IPEndPoint(iPAddress, p);
+                    ipeAgent = new IPEndPoint(iPAddress, p);
                 }
                 catch (Exception e)
                 {
@@ -47,7 +198,7 @@ namespace lcmc
                     IPAddress iPAddress = IPAddress.Parse(s[0]);
                     int p = int.Parse(s[1]);
 
-                    ipeEngineer = new IPEndPoint(iPAddress, p);
+                    ipeConsumer = new IPEndPoint(iPAddress, p);
                 }
                 catch (Exception e)
                 {
@@ -55,94 +206,14 @@ namespace lcmc
                 }
             }
 
+            Thread t = new Thread(new ThreadStart(listenForAgents));
+            t.Start();
 
-            listenforagents(ipeAgents);
-            listenforagents(ipeEngineer);
+            Thread t2 = new Thread(new ThreadStart(listenForConsumers));
+            t2.Start();
 
-
-            try
-            {
-                Console.WriteLine("press any key to exit...");
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.Write("e: " + e.ToString());
-            }
-        }
-
-        private static void listenforagents(IPEndPoint ipe)
-        {
-            Socket listener = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            listener.Bind(ipe);
-
-            listener.Listen(100);
-            Console.WriteLine("listening on socket {0}:{1}",
-                ipe.Address.ToString(),
-                ipe.Port
-                );
-
-
-            listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
-        }
-        static void AcceptCallback(IAsyncResult ar)
-        {
-            // Get the socket that handles the client request.  
-            Socket listener = (Socket)ar.AsyncState;
-            Socket handler = listener.EndAccept(ar);
-            handler.NoDelay = true;
-
-            // Signal the main thread to continue.  
-            //allDone.Set();
-
-            // Create the state object.  
-            StateObject state = new StateObject();
-            stateObjects.Add(state);
-            state.workSocket = handler;
-            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReadCallback), state);
-
-            Console.WriteLine("client connected to port: {0}", ((IPEndPoint)handler.LocalEndPoint).Port);
-        }
-
-        public static void ReadCallback(IAsyncResult ar)
-        {
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket handler = state.workSocket;
-            handler.NoDelay = true;
-
-
-            int read = handler.EndReceive(ar);
-            Console.Write("{0} {1} bytes",
-                        ((IPEndPoint)state.workSocket.LocalEndPoint).Port == 11000 ? "-P->" : "<-A-",
-                        read
-                        );
-
-            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReadCallback), state);
-
-
-            foreach (StateObject s in stateObjects)
-            {
-                if (((IPEndPoint)s.workSocket.LocalEndPoint).Port + 1000 ==
-                    ((IPEndPoint)state.workSocket.LocalEndPoint).Port
-                    ||
-                    ((IPEndPoint)s.workSocket.LocalEndPoint).Port - 1000 ==
-                     ((IPEndPoint)state.workSocket.LocalEndPoint).Port
-                    )
-                {
-                    // found corresponding stateobject
-                    Console.WriteLine(".");
-
-                    s.workSocket.NoDelay = true;
-                    s.workSocket.Send(state.buffer, read, SocketFlags.None);
-                    break;
-                }
-            }
+            Console.WriteLine("\nPress ENTER to quit...");
+            Console.ReadLine();
         }
     }
-
-
 }
